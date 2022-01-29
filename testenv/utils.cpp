a924bad81bf45f2b1846f99ae79396c16e2a36d0
raj <raj.patwari@gmail.com>
2022-01-28 19:29:20 -0800

    testcase to test boost, eigne, gtest, cmake, gxx

---
+++ testenv/utils.cpp
@@ -0,0 +1,13 @@
+// 
+// @rajeevp
+//
+
+float compute (float a, float b)
+{
+    return a*4.3 + b;
+}
+
+int sum (int a, int b)
+{
+    return a + b;
+}
---
+++ testenv/utils.hpp
@@ -0,0 +1,6 @@
+// 
+// @rajeevp
+//
+
+float compute (float a, float b);
+int sum (int a, int b);
